85d6a86979c32237edacb10d19acd48cf7d3cae9
Otávio Leão <otavioslb2005@gmail.com>
2026-08-17 19:09:15 -0300

    feat: adiciona PAUSE nos exercicios 1 e 2

--- Lista-Cap1/exercicio01.c
+++ Lista-Cap1/exercicio01.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     int anoNasc = 2005;
 
     printf("Meu ano de nascimento: %d\n", anoNasc);
 
+    system("PAUSE");
     return 0;
 }
--- Lista-Cap1/exercicio02.c
+++ Lista-Cap1/exercicio02.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     float ouro = 1.618;
 
     printf("Numero de Ouro: %.3f\n", ouro);
 
+    system("PAUSE");
     return 0;
 }
