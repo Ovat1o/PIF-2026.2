52f0891d11d69bea53a6d5ddb48edd9841323aa6
Otávio Leão <otavioslb2005@gmail.com>
2026-08-26 15:19:50 -0300

    docs: adiciona exercicio 18 completo

---
+++ Lista-Cap1/exercicio18.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    float lapis = 4.88;
+    float borracha = 234.54;
+    float caneta = 42.04;
+    float caderno = 8.00;
+    float fita = 13.05;
+
+    printf("Lapis     %12.2f\n", lapis);
+    printf("Borrachas %12.2f\n", borracha);
+    printf("Canetas   %12.2f\n", caneta);
+    printf("Cadernos  %12.2f\n", caderno);
+    printf("Fitas     %12.2f\n", fita);
+    
+    system("PAUSE");
+    return 0;
+}
