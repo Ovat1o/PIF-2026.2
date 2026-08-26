90a396620de7666706f48760384853838e1a0f17
Otávio Leão <otavioslb2005@gmail.com>
2026-08-26 18:19:01 -0300

    docs: adiciona exercicio 20 completo

---
+++ Lista-Cap1/exercicio20.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    printf("\xC9\xCD\xCD\xBB \n");
+    printf("\xBA  \xBA \n");
+    printf("\xC8\xCD\xCD\xBC \n");
+
+    system("PAUSE");
+    return 0;
+}
