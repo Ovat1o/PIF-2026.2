b9ee1f968946e9c9db4a0f40f7ecbfb6b04621b6
Otávio Leão <otavioslb2005@gmail.com>
2026-08-26 19:30:17 -0300

    docs: adiciona exercicio 25 completo

---
+++ Lista-Cap1/exercicio25.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("CCCCC\n");
+    printf("C    \n");
+    printf("C    \n");
+    printf("CCCCC\n");
+
+    system("PAUSE");
+    return 0;
+}
