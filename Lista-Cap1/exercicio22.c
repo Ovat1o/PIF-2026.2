2e1d9d3436d4e20431251c2edfa2d652183a890e
Otávio Leão <otavioslb2005@gmail.com>
2026-08-26 19:20:50 -0300

    docs: adiciona exercicio 22 completo

---
+++ Lista-Cap1/exercicio22.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // Carro
+    printf("\xDC\xDC\xDB\xDB\xDB\xDB\xDC\xDC\n");
+    printf("\xDFO\xDF\xDF\xDF\xDF\xDFO\xDF\n\n");
+
+    // Caminhonete
+    printf("\xDC\xDC\xDB \xDB\xDB\xDB\xDB\xDB\xDB\n");
+    printf("\xDFO\xDF\xDF\xDF\xDF\xDFOO\xDF\n\n");
+
+    system("PAUSE");
+    return 0;
+}
